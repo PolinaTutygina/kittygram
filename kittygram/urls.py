c40c1d07d74ffa6a63a7a5d61c9e3462f55f70dc
Polina Tutygina <polina.tutygina@yandex.ru>
2025-01-05 11:23:28 +0300

    Views are implemented.

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,45 @@
-from django.urls import path
+# Роутеры.
+from rest_framework.routers import SimpleRouter
+from django.urls import include, path
+from cats.views import CatViewSet
 
-from cats.views import cat_list
+# Создаётся роутер
+router = SimpleRouter()
+# Вызываем метод .register с нужными параметрами
+router.register('cats', CatViewSet)
+# В роутере можно зарегистрировать любое количество пар "URL, viewset":
+# например
+# router.register('owners', OwnerViewSet)
+# Но нам это пока не нужно
 
 urlpatterns = [
-   path('cats/', cat_list),
+    # Все зарегистрированные в router пути доступны в router.urls
+    # Включим их в головной urls.py
+    path('', include(router.urls)),
 ]
+
+
+# Высокоуровневые view-классы (дженерики).
+#from cats.views import CatList, CatDetail
+#
+#urlpatterns = [
+#    path('cats/', CatList.as_view()),
+#    path('cats/<int:pk>/', CatDetail.as_view()),
+#] 
+
+
+# Низкоуровневый view-класс.
+#from cats.views import APICat
+#
+#urlpatterns = [
+#    path('cats/', APICat.as_view()),
+#]
+
+# View-функция.
+#from cats.views import cat_list
+#
+#urlpatterns = [
+#   path('cats/', cat_list),
+#]
 
 
